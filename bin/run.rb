b13fa4e8ffc89d23f411643b372ec2000bb45253
hellosylvee <helloimsylvia@gmail.com>
2017-03-29 16:35:43 -0400

    updated attributes in api communicator

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,8 @@
-require_relative 'config/environment'
+require_relative '../config/environment.rb'
 
-require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
+require_relative "../lib/api_communicator.rb"
 
 welcome
 pokemon = get_pokemon_name_from_user
-show_pokemon_details(pokemon)
+poke_info(pokemon)
