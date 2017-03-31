bade5b963954b11b5c9aff42c4e15877ab4c8fd4
peterchic <peterchicarielli@gmail.com>
2017-03-31 00:10:23 -0400

    type change version 2

--- db/migrate/20170328195241_create_pokemons.rb
+++ db/migrate/20170328195241_create_pokemons.rb
@@ -2,7 +2,7 @@ class CreatePokemons < ActiveRecord::Migration[5.0]
   def change
     create_table :pokemons do |t|
       t.string :name
-      t.string :type
+      t.string :poke_type
       t.integer :base_xp
     end
   end
